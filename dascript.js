aa9aba905f4d1b6a9ca0227814ea89ad9eb64565
Fifi Arisandi <fifi_arisandi@yahoo.com>
2021-11-23 01:11:16 +0200

    tidy up2

--- dascript.js
+++ dascript.js
@@ -7,14 +7,23 @@
   } */
 
 
-function clickedBtn() {
+/*let result = document.querySelector("#sponsorText");
+
+document.addEventListener('click', (e) => {
+    let element = e.target;
+    if(element.tagName == "BUTTON"){
+        result.innerText = `${element.id}: ${element.innerText}`;
+    }
+} */
+
+ function clickedBtn() {
     document.getElementById("dogsBtn").addEventListener("click", dButton(e));
     //document.getElementById("dogsBtn").onclick = dButton;
 }
 
 function dButton(e) {
     //document.getElementsByName("sponsorText")[0].value="";
-    if (e.target.tagName == 'BUTTON') {
+    if (e.target.tagName == "BUTTON") {
         var dogName = e.target.name;
         document.getElementById("sponsorText").innerHTML = "I want to sponsor " + e.target.name;
       
@@ -24,7 +33,6 @@ function dButton(e) {
 }
 
 
- 
 
  /* function inputSponsorTextBox() { 
       
